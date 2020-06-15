0d225a70e14e9719957a654d813b7c80b48df200
Jochem de Wit <jochemdewit49@gmail.com>
2020-06-15 23:52:13 +0200

    Profile icon + menu fixed

--- public/js/master.js
+++ public/js/master.js
@@ -1,3 +1,20 @@
 // Entrance animations
 // var ea = gsap.timeline();
 // gsap.to('.nav-logo', .5, {opacity: 0});
+
+var profile = document.querySelector('#profile');
+var profileClose = document.querySelector('#profile-close')
+var cart = document.querySelector('cart');
+
+var openProfile = gsap.timeline({paused: true})
+openProfile.set('#profile-menu', {display: "block"});
+openProfile.to('#profile-menu', .3, {opacity: 1, ease: "power2.out"});
+
+
+profile.addEventListener('click', function(){
+    openProfile.play();
+});
+profileClose.addEventListener('click', function(){
+    openProfile.reverse();
+});
+
